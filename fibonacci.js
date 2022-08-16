ecbf307267fc981cc94009a9acb21287dab03290
Daniel <dwgrossberg@gmail.com>
2022-08-16 16:09:05 -0400

    Start mergeSort algo

--- fibonacci.js
+++ fibonacci.js
@@ -2,7 +2,7 @@ function fibs(num) {
   let seq = [0, 1];
   let a = 0;
   let b = 1;
-  for (let i = 1; i < num; i++) {
+  for (let i = 2; i < num; i++) {
     let c = a + b;
     seq.push(c);
     a = b;
@@ -14,13 +14,9 @@ function fibs(num) {
 console.log(fibs(20));
 
 function fibsRecursive(num) {
-  if (num < 2) {
-    return [1];
+  if (num <= 2) {
+    return [0, 1];
   }
-  if (num < 3) {
-    return [1, 1];
-  }
-
   let a = fibsRecursive(num - 1);
   a.push(a[num - 2] + a[num - 3]);
   return a;
---
+++ mergeSort.js
@@ -0,0 +1,24 @@
+const mergeSort = (array) => {
+  if (array.length < 2) return array;
+  else {
+    let half = Math.ceil(array.length / 2);
+    let firstHalf = array.slice(0, half);
+    let secondHalf = array.slice(half);
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf));
+  }
+};
+
+const merge = (firstArray, secondArray) => {
+  let sorted = [];
+  while (firstArray.length > 0 && secondArray.length > 0) {
+    firstArray[0] > secondArray[0]
+      ? sorted.push(secondArray[0])
+      : sorted.concat(firstArray[0]);
+  }
+
+  return sorted;
+};
+
+const unsorted = [93, 151, 3, 20, 463, 155, 167, 34, 13];
+
+console.log(mergeSort(unsorted));
